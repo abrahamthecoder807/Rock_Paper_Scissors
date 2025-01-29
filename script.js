68e75028f8b013f3ff73acdf7266fb13996812a8
abrahamthecoder807 <abrahamj1807@gmail.com>
2025-01-29 19:09:53 +0100

    Listed out the steps I will take to solve this project and also create the getcomputerchoice function.

--- script.js
+++ script.js
@@ -5,3 +5,40 @@
 // 4. Declare the player score variables.
 // 5. Write the logic to play a single round.
 // 6. Write the logic to play the entire game.
+
+// #Step 2
+// create a function that gets the computer choice
+// the function will return one of the string values; either rock, paper or scissors.
+// test that the function returns what i expect it to return using console.log or use the browser developer tools
+
+// Step 3
+// create a function the gets the human choice
+// this functio will also return one of the valid choices depending on what the use input.
+// test what the function returns by using console.log.
+
+// Step 4
+// Declare the players score variables. and write variables to keep track of the players score/
+// create two new variables named humanscore and computerscore in the global scope
+// initialize those variables with the values of 0.
+
+// Step 5
+// create a logic to play a single round.
+// create a function named playRound.
+// define two parameters for playRound: humanchhoice and computerchoice. use these two parameter to take the human and computer as argument
+
+console.log("Hello world!");
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else (randomNumber === 2) 
+        return "scissors";
+}
+
+const f = getComputerChoice();
+
+console.log(f);
