0e05313805a6466f9d49864a3421476f04466e17
abrahamthecoder807 <abrahamj1807@gmail.com>
2025-01-29 04:20:36 +0100

    Writing down my palns and how I intent to tackle the project

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// 1. Step up the Git repository/Workspace.
+// 2. Write the logic to get the computer choice
+// 3. Write the logic to get the human choice.
+// 4. Declare the player score variables.
+// 5. Write the logic to play a single round.
+// 6. Write the logic to play the entire game.
