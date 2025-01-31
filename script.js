12ca16ec905e4405449a79d209beeb9c3b85a69f
abrahamthecoder807 <abrahamj1807@gmail.com>
2025-01-31 01:30:11 +0100

    Remove the unnecessary lines of code in the playRound function and made it more concise and redable.

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
 
-// 1. Step up the Git repository/Workspace.
-// 2. Write the logic to get the computer choice
-// 3. Write the logic to get the human choice.
-// 4. Declare the player score variables.
-// 5. Write the logic to play a single round.
-// 6. Write the logic to play the entire game.
-
-// #Step 2
-// create a function that gets the computer choice
-// the function will return one of the string values; either rock, paper or scissors.
-// test that the function returns what i expect it to return using console.log or use the browser developer tools
-
-// Step 3
-// create a function the gets the human choice
-// this function will also return one of the valid choices depending on what the user input.
-// test what the function returns by using console.log.
-
-// Step 4
-// Declare the players score variables. and write variables to keep track of the players score/
-// create two new variables named humanscore and computerscore in the global scope
-// initialize those variables with the values of 0.
-
-// Step 5
-// create a logic to play a single round.
-// create a function named playRound.
-// define two parameters for playRound: humanchhoice and computerchoice. use these two parameter to take the human and computer as argument
-
 let humanScore = 0;
 let computerScore = 0;
 
-console.log("Hello world!");
+
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -44,41 +17,36 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+    // const prompt = require('prompt-sync')();
     const playerChoice = prompt("Please choose between rock, paper or scissors: ").toLowerCase();
         return playerChoice;
 } 
 
 function playRound(humanChoice, computerChoice) {
-    for (; humanScore < 3; humanScore++) {
-        if (humanChoice === "rock" && computerChoice === "rock") {
-            console.log("It's a tie try again");
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            computerScore++;
-            console.log("Computer wins this round!");
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore++;
+        
+        if (humanChoice === "rock" && computerChoice === "scissors") {
             console.log("You won this round! rock beats scissors");
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
             humanScore++;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
             console.log("You won this round! paper beats rock")
-        } else if (humanChoice === "paper" && computerChoice === "paper") {
-            console.log("It's a tie try again");
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore++;
-            console.log("Computer wins this round!");
+            humanScore++;
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log("You won this round! scissors beats paper")
+            humanScore++;
+        } else if (humanChoice === computerChoice) {
+            console.log("It's a tie try again!")
+        } else {
+                console.log("You lose " + computerChoice + " beats " + humanChoice);
+                computerScore++;
         }
-    
-    }
-    console.log(humanScore);
-    
 }
 
 const computerSelection = getComputerChoice();
 const humanSelection = getHumanChoice();
 
-console.log(computerSelection)
 console.log(humanSelection)
+console.log(computerSelection)
+console.log(`Computer score is ${computerScore}`);
+console.log(`Human score is ${humanScore}`)
 
-console.log(computerScore);
-
-playRound(computerSelection ,humanSelection);
+console.log(playRound(humanSelection, computerSelection));
